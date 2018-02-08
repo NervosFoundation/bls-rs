42d26a45a4bd25b4ba0e72e578dfdd81aea824c8
zhangsoledad <787953403@qq.com>
2018-02-08 11:55:33 +0800

    refactor(complie): clean build out dir
    upgrade gcc
    
    build

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 extern crate cc;
+extern crate num_cpus;
 
 use std::env;
 use std::fs;
@@ -68,55 +69,67 @@ fn build_gmp() {
     let build = dst.join("libgmp");
     t!(fs::create_dir_all(&build));
     cp_r(&src, &build);
-
     run_command_or_fail(&build, "./configure", &[]);
-    run_command_or_fail(&build, "make", &[]);
-    run_command_or_fail(&build, "make", &["check"]);
-    println!("cargo:rustc-link-search=native={}/.libs", build.display());
-    println!("cargo:rustc-link-lib=static=gmp");
+    run_command_or_fail(&build, "make", &["-j", &num_cpus::get().to_string()]);
 }
 
 fn build_pbc() {
-    let src = env::current_dir()
-        .expect("Can't find current dir")
-        .join("libpbc");
+    let current_dir = env::current_dir()
+        .expect("Can't find current dir");
+    
+    let pbc_src = current_dir.join("libpbc");
     let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let gmp = dst.join("libgmp");
-    let build = dst.join("libpbc");
+    let pbc = dst.join("libpbc");
 
-    t!(fs::create_dir_all(&build));
-    cp_r(&src, &build);
+    let libs = dst.join("libs");
+    let include_dir = dst.join("include");
 
-    let ld = format!("-L{}", gmp.join(".libs").display());
-    let include = format!("-I{}", gmp.display());
+    t!(fs::create_dir_all(&pbc));
+    t!(fs::create_dir_all(&libs));
+    t!(fs::create_dir_all(&include_dir));
+    cp_r(&pbc_src, &pbc);
+    cp_r(&pbc_src.join("include"), &include_dir);
+    t!(fs::copy(gmp.join("gmp.h"), include_dir.join("gmp.h")));
+    t!(fs::copy(gmp.join(".libs/libgmp.a"), libs.join("libgmp.a")));
+
+    let ld = format!("-L{}", libs.display());
+    let include = format!("-I{}", include_dir.display());
 
     let mut configure_flags = Vec::new();
     configure_flags.push("--enable-shared=no");
     configure_flags.push("--enable-static=yes");
+    configure_flags.push("--with-pic");
 
-    run_command_or_fail(&build, "./setup", &[]);
+    env::set_var("PBC_CPPFLAGS", &include);
+    run_command_or_fail(&pbc, "./setup", &[]);
 
     Command::new("./configure")
-        .current_dir(&build)
+        .current_dir(&pbc)
         .env("LDFLAGS", &ld)
+        .env("CFLAGS", &include)
         .args(&configure_flags)
         .status()
         .expect("failed to execute process");
     Command::new("make")
-        .current_dir(&build)
+        .current_dir(&pbc)
         .env("LDFLAGS", &ld)
         .env("CFLAGS", &include)
+        .args(&["-j", &num_cpus::get().to_string()])
         .status()
         .expect("failed to execute process");
+    t!(fs::copy(pbc.join(".libs/libpbc.a"), libs.join("libpbc.a")));
 
-    println!("cargo:rustc-link-search=native={}/.libs", build.display());
+    println!("cargo:rustc-link-search=native={}", libs.display());
+    println!("cargo:include={}", include_dir.display());
     println!("cargo:rustc-link-lib=static=pbc");
+    println!("cargo:rustc-link-lib=static=gmp");
 
-    let include_dir = format!("{}/include", build.display());
     cc::Build::new()
         .file("src/bls.c")
-        .include(&gmp)
         .include(&include_dir)
         .static_flag(true)
         .compile("libbls.a");
+    t!(fs::remove_dir_all(&gmp));
+    t!(fs::remove_dir_all(&pbc));
 }
